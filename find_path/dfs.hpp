79fa16590dd2e6d8f2e54c718396228fb116b577
fpolushkin <polushkin.fedor@gmail.com>
2025-03-31 15:43:57 +0300

    добавлены разные приоритеты обхода по DFS. Реализован базовый класс dynamic array и TStack теперь наследуется от него

--- find_path/dfs.hpp
+++ find_path/dfs.hpp
@@ -1,26 +1,49 @@
 #pragma once
 #include "user_stack.hpp"
 #include "map_parsing.hpp"
+#include <ctime>
 
 // The sides of bypassing the DFS algorithm
 enum class sides {
 	left, down, right, up, no_side
 };
 using enum sides;
+// Type of porecudure (clockwise, counterclockwise, random)
+enum class type_procedure {
+	clockwise, counterclockwise, random
+};
+using enum type_procedure;
+constexpr size_t crawl_procedure_len = 3; // max element of crawl_procedure array index
 
 /// <summary>
 /// Description: The step of the DFS algorithm (depths-first search).
-/// The crawl procedure:
-/// 1. Attempt to step to the left
-/// 2. Trying to step down
-/// 3. Attempt to step to the right
-/// 4. Trying to step up
+/// The crawl procedure is randomized
 /// </summary>
 /// <param name="cur_map">a map containing the available movements</param>
 /// <param name="solution">current solution</param>
 /// <param name="solution_find">a flag certifying that a solution has been found</param>
-void dfs_step(const map_type& cur_map, TStack<node>& solution, bool& solution_find) {
-	const std::vector<sides> crawl_procedure = { left , down , right , up }; // The order of traversing the array by dfs
+/// <param name="proc_type">type of porecudure (clockwise, counterclockwise, random)</param>
+void dfs_step(const map_type& cur_map, TStack<node>& solution, bool& solution_find, type_procedure proc_type) {
+	std::vector<sides> crawl_procedure = { up , down , left , right };
+
+#pragma region Rules of procedure
+	if (proc_type == random) { // Random order
+		srand(time(NULL));
+		for (size_t index = 0; index < crawl_procedure_len; ++index) {
+			size_t random_el_index = rand() % (crawl_procedure_len + 1);
+			sides temp_side = crawl_procedure[random_el_index];
+			crawl_procedure[random_el_index] = crawl_procedure[index];
+			crawl_procedure[index] = temp_side;
+		}
+	}
+	else if (proc_type == counterclockwise) { // Counterclockwise order, started by left direction
+		crawl_procedure = { left , up, right , down };
+	}
+	else if (proc_type == clockwise) { // Clockwise order, started by right direction
+		crawl_procedure = { right , down, left , up };
+	}
+#pragma endregion
+
 	for (const auto& side : crawl_procedure) {
 		if (solution_find == true) continue;
 
@@ -49,7 +72,7 @@ void dfs_step(const map_type& cur_map, TStack<node>& solution, bool& solution_fi
 			el_type next_el = cur_map[next_step.y][next_step.x];
 			if (next_el == AVAILABLE) { // If available, continue the crawl
 				solution.push(next_step);
-				dfs_step(cur_map, solution, solution_find);
+				dfs_step(cur_map, solution, solution_find, proc_type);
 			}
 			else if (next_el == END) { // At the end, we expand the recursion
 				solution.push(next_step);
@@ -60,7 +83,7 @@ void dfs_step(const map_type& cur_map, TStack<node>& solution, bool& solution_fi
 		if (solution_find) return; // Recursion reversal condition
 	}
 	// Removing an item from the solution list if further depth-first traversal is not possible
-	solution.pop(); 
+	solution.pop();
 	return;
 }
 
@@ -68,8 +91,9 @@ void dfs_step(const map_type& cur_map, TStack<node>& solution, bool& solution_fi
 /// Description: Depth-first search algorithm for a map from a file (DFS)
 /// </summary>
 /// <param name="map_path">the path to the file containing the map</param>
+/// <param name="proc_type">type of porecudure (clockwise, counterclockwise, random)</param>
 /// <returns>solving the problem of finding a path</returns>
-TStack<node> dfs(std::filesystem::path map_path) {
+TStack<node> dfs(std::filesystem::path map_path, type_procedure proc_type = clockwise) {
 	// Getting a map from a file
 	std::pair<map_type, node> map_parsing_res = map_parsing(map_path);
 	map_type cur_map = map_parsing_res.first;
@@ -79,7 +103,7 @@ TStack<node> dfs(std::filesystem::path map_path) {
 	TStack<node> solution(cur_map.size());
 	solution.push(start_node);
 	bool solution_find = false;
-	dfs_step(cur_map, solution, solution_find);
+	dfs_step(cur_map, solution, solution_find, proc_type);
 
 	// If there is no solution, we throw an exception.
 	if (not solution_find) throw "Solution not found"; 
@@ -89,16 +113,17 @@ TStack<node> dfs(std::filesystem::path map_path) {
 /// <summary>
 /// Description: Depth-first search Algorithm for a given Map (DFS)
 /// </summary>
-/// <param name="map_area">a map containing the available movements</param>
+/// <param name="map_area">map containing the available movements</param>
+/// <param name="proc_type">type of porecudure (clockwise, counterclockwise, random)</param>
 /// <returns>solving the problem of finding a path</returns>
-TStack<node> dfs(std::pair<map_type, node> map_area) {
+TStack<node> dfs(std::pair<map_type, node> map_area, type_procedure proc_type = clockwise) {
 	map_type cur_map = map_area.first;
 	node start_node = map_area.second;
 
 	TStack<node> solution(cur_map.size());
 	solution.push(start_node);
 	bool solution_find = false;
-	dfs_step(cur_map, solution, solution_find);
+	dfs_step(cur_map, solution, solution_find, proc_type);
 
 	if (not solution_find) throw "Solution not found";
 	else return solution;
--- find_path/user_stack.hpp
+++ find_path/user_stack.hpp
@@ -1,80 +1,99 @@
 #pragma once
 
-template <typename stack_type>
-class TStack {
-private:
-	stack_type* stack_body;  // Stack Contents
-	size_t body_size;        // The maximum number of elements that can be in the current memory allocated for the stack
-	size_t stack_size;       // Number of items in the stack
+template <typename dyn_array_type>
+class TDynamic_array {
+public:
+	dyn_array_type* body;  // Dynamic array contents
+	size_t body_size;      // The maximum number of elements that can be in the current memory allocated for the dynamic array
+	size_t body_len;       // Number of items in the dynamic array
 
+	// Creating a dynamic array with allocated memory for 1 element
+	TDynamic_array() { 
+		this->body = new dyn_array_type[1];
+		this->body_len = 0;
+		this->body_size = 1;
+	};
+
+	// Creating a dynamic array with allocated memory for n-elements
+	TDynamic_array(size_t n) {
+		this->body = new dyn_array_type[n];
+		this->body_len = 0;
+		this->body_size = n;
+	};
+
+	~TDynamic_array() {
+		delete this->body;
+	}
+
+	// Changing the allocated memory for the stack
+	void resize(size_t n) {
+		if (this->body_len > n) { // When trying to make a size reduction, we throw an exception
+			throw "Resize error: an attempt to reduce the size";
+		}
+
+		dyn_array_type* temp_body = new dyn_array_type[n];
+		for (size_t i = 0; i < this->body_len; ++i) {
+			temp_body[i] = body[i];
+		}
+		delete body;       // Deleting the old array bound to the stack body
+		body = temp_body;  // Reconnecting to the memory that the temporary pointer points to
+		this->body_size = n;
+	};
+
+	virtual bool empty() const = 0;
+};
+
+template <typename stack_type>
+class TStack : TDynamic_array<stack_type> {
 public:
 	TStack() {
-		this->stack_body = new stack_type[1];
-		this->stack_size = 0;
+		this->body = new stack_type[1];
+		this->body_len = 0;
 		this->body_size = 1;
 	};
 
 	// Creating a stack with allocated memory for n-elements
 	TStack(size_t n) {
-		this->stack_body = new stack_type[n];
-		this->stack_size = 0;
-		this->body_size = n;
-	};
-
-	~TStack() {
-		delete this->stack_body;
-	}
-
-	// Changing the allocated memory for the stack
-	void resize(size_t n) {
-		if (this->stack_size > n) { // When trying to make a size reduction, we throw an exception
-			throw "Resize error: an attempt to reduce the size";
-		}
-
-		stack_type* temp_body = new stack_type[n];
-		for (size_t i = 0; i < this->stack_size; ++i) {
-			temp_body[i] = stack_body[i];
-		}
-		delete stack_body;       // Deleting the old array bound to the stack body
-		stack_body = temp_body;  // Reconnecting to the memory that the temporary pointer points to
+		this->body = new stack_type[n];
+		this->body_len = 0;
 		this->body_size = n;
 	};
 
 	// Removing the top element of the stack
 	stack_type pop() {
-		if (this->stack_size == 0) throw "Size error: an attempt to delete an item for an empty stack";
-		this->stack_size--;
-		return this->stack_body[this->stack_size];
+		if (this->body_len == 0) throw "Size error: an attempt to delete an item for an empty stack";
+		this->body_len--;
+		return this->body[this->body_len];
 	}
 
 	// Adding an item to the top of the stack
 	void push(stack_type& new_el) {
-		if (this->body_size < this->stack_size+1) {
-			this->resize(this->stack_size * 2);
+		if (this->body_size < this->body_len+1) {
+			this->resize(this->body_len * 2);
 		}
-		this->stack_body[stack_size++] = new_el;
+		this->body[this->body_len++] = new_el;
 	}
 
 	// Viewing the top element of the stack
 	stack_type top() const {
-		if (this->stack_size == 0) throw "Size error: an attempt to get an item for an empty stack";
-		return this->stack_body[this->stack_size-1];  // Return of the top element of the stack
+		if (this->body_len == 0) throw "Size error: an attempt to get an item for an empty stack";
+		return this->body[this->body_len-1];  // Return of the top element of the stack
 	}
 
 	// Checking for emptiness
-	bool empty() const  {
-		return not((bool)this->stack_size);  // If the stack size is not zero, then it is not empty
+	bool empty() const override {
+		return not((bool)this->body_len);  // If the stack size is not zero, then it is not empty
 	}
 
 	// Current stack size (number of items)
-	size_t size() const  {
-		return this->stack_size;
+	size_t size() const {
+		return this->body_len;
 	}
 
 	// Checking for the content of an element
 	bool contain(stack_type& el) const {
-		for (size_t i = 0; i < this->stack_size; ++i) {
-			if (el == this->stack_body[i]) return true;
+		for (size_t i = 0; i < this->body_len; ++i) {
+			if (el == this->body[i]) return true;
 		}
 		return false;
 	}
